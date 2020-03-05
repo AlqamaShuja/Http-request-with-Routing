bdc6e074062b355ff503a49149df8bed1c5455c9
Alqama Shuja <alqamashuja313@gmail.com>
2020-03-06 03:38:39 +0500

    http ajax with routing

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post";
+import FullPost from "../FullPost/FullPost";
+import "./Posts.css";
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Alqama"
+          };
+        });
+        this.setState({
+          posts: updatedPosts
+        });
+      })
+      .catch(error => {
+        console.log("error: ", error);
+        // this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler = id => {
+    this.props.history.push({ pathname: "/posts/" + id });
+    // this.props.history.push('/posts' + id);
+  };
+
+  render() {
+    let posts = <p style={{ textAlign: "center" }}> Something went wrong..</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          // <Link to={"/posts" + post.id} >
+          <Post
+            key={post.id}
+            clicked={() => this.postSelectedHandler(post.id)}
+            title={post.title}
+            author={post.author}
+          />
+          // </Link>
+        );
+      });
+    }
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        {/* <Route path="/posts/:id" exact component={FullPost} /> */}
+        <Route
+          path={this.props.match.url + "/:id"}
+          exact
+          component={FullPost}
+        />
+      </div>
+    );
+  }
+}
+
+export default Posts;
